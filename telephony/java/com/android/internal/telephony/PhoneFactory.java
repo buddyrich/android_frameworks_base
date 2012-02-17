45d228a3257d77195caae19f667d2f318de38b69
dhacker29 <davidhackerdvm@gmail.com>
2012-02-16 23:34:28 -0600

    fix borked merge

--- telephony/java/com/android/internal/telephony/PhoneFactory.java
+++ telephony/java/com/android/internal/telephony/PhoneFactory.java
@@ -170,6 +170,7 @@ public class PhoneFactory {
                 } else if ("motow3g".equals(sRILClassname)) {
                     Log.i(LOG_TAG, "Using Motorola Wrigley 3G RIL");
                     sCommandsInterface = new MotoWrigley3GRIL(context, networkMode, cdmaSubscription);
+);
                 } else {
                     sCommandsInterface = new RIL(context, networkMode, cdmaSubscription);
                 }
